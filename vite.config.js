b9151ad7b6f5a5b7c72532df75c458f942eb1e7c
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-20 12:59:47 +0100

    fix: some errors

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [
@@ -11,4 +11,4 @@ export default defineConfig({
         }),
         react(),
     ],
-});
+})
